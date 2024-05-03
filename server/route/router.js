4a1519adb90ed46de635fdfa6031459c69e277fd
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-05-03 23:08:16 +0545

    added editAndDeleteBlog

--- server/route/router.js
+++ server/route/router.js
@@ -1,11 +1,13 @@
-const express = require("express")
-const router = express.Router()
-const {registerFunc,loginUser, addBlog,allBlog, userInfo} = require("../controller/controller")
+const express = require("express");
+const router = express.Router();
+const { registerFunc, loginUser, addBlog, allBlog, userInfo, editBlog, delBlog } = require("../controller/controller");
 
-router.route("/register").post(registerFunc)
-router.route("/login").post(loginUser)
-router.route("/add-blog").post(addBlog)
-router.route("/all-blog").get(allBlog)
-router.route("/user-info").get(userInfo)
+router.route("/register").post(registerFunc);
+router.route("/login").post(loginUser);
+router.route("/add-blog").post(addBlog);
+router.route("/all-blog").get(allBlog);
+router.route("/user-info").get(userInfo);
+router.route("/edit-blog/:blogId").patch(editBlog); // Note: I added ":blogId" as a route parameter
+router.route("/del-blog/:blogId").delete(delBlog)
 
-module.exports = router
+module.exports = router;
